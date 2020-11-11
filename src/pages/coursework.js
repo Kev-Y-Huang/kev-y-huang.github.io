71827a79c2efb3b7affae8877b056256ba361342
Kev-Y-Huang <kev.y.huang@gmail.com>
2020-11-10 23:55:19 -0600

    Some changes

--- src/pages/coursework.js
+++ src/pages/coursework.js
@@ -3,42 +3,75 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const CourseworkPage = () => (
-  <Layout>
-    <SEO title="Coursework Page"/>
-    <h1>Coursework:</h1>
-    <h2>Spring 2020</h2>
-    <ul>
-      <li><a href={"https://www.math.harvard.edu/course/mathematics-25b/"}>Math 25b</a>: Theoretical Linear Algebra and
-        Real Analysis II
-      </li>
-      <li><a href={"http://sites.fas.harvard.edu/~libcs124/cs124/index.html"}>CS 124</a>: Data Structures and Algorithms
-      </li>
-      <li><a href={"https://www.vox.com/the-highlight/2019/5/14/18520783/harvard-economics-chetty"}>Econ 50</a>: Using
-        Big Data to Solve Economic and Social Problems
-      </li>
-      <li><a href={"https://gened.fas.harvard.edu/classes/ignorance-lies-hogwash-and-humbug"}>Gened 1023</a>: Ignorance,
-        Lies, Hogwash, and Humbug
-      </li>
-    </ul>
-    <h2>Fall 19</h2>
-    <ul>
-      <li>
-        <a href={"https://www.math.harvard.edu/course/mathematics-25a/"}>Math 25a</a>: Theoretical Linear Algebra and
-        Real Analysis I
-      </li>
-      <li>
-        <a href={"https://cs50.harvard.edu/"}>CS 50</a>: Introduction to Computer Science
-      </li>
-      <li>
-        <a href={"https://writingprogram.fas.harvard.edu/pages/expos-20-0"}>Expos 20</a>: Privacy and Surveillance
-      </li>
-      <li>
-        <a href={"https://scholar.harvard.edu/mbaum/classes/frsemr-41r-freshman-seminar-media-american-politics"}>Freshman
-          Seminar 41R</a>: Media in American Politics
-      </li>
-    </ul>
-  </Layout>
-)
+import { Box } from "@material-ui/core"
+
+function CourseworkPage() {
+  const courses = [
+    {
+      "name": "Spring 2020",
+      "courses": [
+        {
+          "course": "Math 25b",
+          "link": "https://www.math.harvard.edu/course/mathematics-25b/",
+          "description": "Theoretical Linear Algebra and Real Analysis II"
+        }, {
+          "course": "CS 124",
+          "link": "http://sites.fas.harvard.edu/~libcs124/cs124/index.html",
+          "description": "Data Structures and Algorithms"
+        }, {
+          "course": "Econ 50",
+          "link": "https://www.vox.com/the-highlight/2019/5/14/18520783/harvard-economics-chetty",
+          "description": "Using Big Data to Solve Economic and Social Problems"
+        }, {
+          "course": "Gened 1023",
+          "link": "https://gened.fas.harvard.edu/classes/ignorance-lies-hogwash-and-humbug",
+          "description": "Ignorance, Lies, Hogwash, and Humbug"
+        }
+      ]
+    }, {
+      "name": "Fall 2019",
+      "courses": [
+        {
+          "course": "Math 25a",
+          "link": "https://www.math.harvard.edu/course/mathematics-25a/",
+          "description": "Theoretical Linear Algebra and Real Analysis I"
+        }, {
+          "course": "CS 50",
+          "link": "https://cs50.harvard.edu/",
+          "description": "Introduction to Computer Science"
+        }, {
+          "course": "Expos 50",
+          "link": "https://writingprogram.fas.harvard.edu/pages/expos-20-0",
+          "description": "Privacy and Surveillance"
+        }, {
+          "course": "Freshman Seminar 41R",
+          "link": "https://scholar.harvard.edu/mbaum/classes/frsemr-41r-freshman-seminar-media-american-politics",
+          "description": "Media in American Politics"
+        }
+      ]
+    }
+  ]
+
+  return (
+    <Layout>
+      <SEO title="Coursework Page"/>
+      <h1>Coursework:</h1>
+      {courses && courses.map(semester => (
+        <Box>
+          <h2>{semester.name}</h2>
+          <ul>
+            {semester && semester.courses.map(course => (
+              <li>
+                <a href={course.link}>
+                  {course.course}
+                </a>: {course.description}
+              </li>
+            ))}
+          </ul>
+        </Box>
+      ))}
+    </Layout>
+  )
+}
 
 export default CourseworkPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,22 @@ const IndexPage = () => (
       />
     </h1>
     <div className={"description"}>
-      I'm a <b>rising sophomore</b> at <b>Harvard University</b> planning to study <b>computer
-      science</b> and <b>math</b>.
-      Outside of class, I'm technical director of <a href={"http://hodp.org/"}>Harvard Open Data Project</a>,
-      a principle in <a href={"https://www.harvardcap.org/"}>Harvard Undergraduate Capital Partners</a>,
-      and a developer for <a href={"https://datamatch.me/"}>Datamatch</a>.
-      I'm passionate about coding, all things tech, and music.
+      <div>
+        I'm a <b>rising sophomore</b> at <b>Harvard University</b> planning to study <b>computer
+        science</b> and <b>math</b>.
+      </div>
+      <br/>
+      <div>
+        Outside of class, I'm technical director of <a href={"http://hodp.org/"}>Harvard Open Data Project</a>,
+        a principal in <a href={"https://www.harvardcap.org/"}>Harvard Undergraduate Capital Partners</a>,
+        and a developer for <a href={"https://datamatch.me/"}>Datamatch</a>.
+        I'm passionate about all things tech, music, and cycling.
+      </div>
+      <br/>
+      <div>
+        Currently, I'm taking a <b>gap year</b> and will be working at <b>Shuttefly</b> as a <b>Software Developer Contractor</b>
+        while continuing to work with Harvard clubs/organizations and coding personal projects.
+      </div>
     </div>
   </Layout>
 )
