17b1c5826d7b41a526299bba5d465cea58670936
Kev-Y-Huang <Kev-Y-Huang@users.noreply.github.com>
2023-07-27 06:09:04 +0000

    deployed via Gatsby Publish Action 🎩 for 28a91e4fdaf9dd14332c9a726028ed8c082b697e

---
+++ component---src-pages-404-js-021f6d9cd4279d5d7eed.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkev_y_huang_github_io=self.webpackChunkkev_y_huang_github_io||[]).push([[883],{45:function(e,t,n){n.r(t);var u=n(7294),l=n(8002),a=n(3432);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-021f6d9cd4279d5d7eed.js.map
